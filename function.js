3a11e5a4236d699f6d816782013557676f7f0542
Louis Caruso <carusolouis00@gmail.com>
2020-06-10 20:48:52 -0500

    update with loop

--- function.js
+++ function.js
@@ -1,56 +1,63 @@
-function askName() {
-    var username = prompt('What is your name?');
-    return document.write('<h3>Hello ' + username + '</h3>');
+
+  var username = prompt('What is your name traveler?');
+  document.write('<h3>Welcome' + username + '</h3>');  
+
+var preference = prompt('Are you with the imperials or stormcloaks?')
+var message;
+
+if (preference === 'imperials'){
+    message = 'The legion welcomes all the swords we can get!';
+  } else if (preference === 'stormcloaks'){
+    message = 'Leave!';
+  } else {
+    message = 'I did not want to know anyway.';
+  }
+  document.write('<h3>' + message + '</h3>')
+  var today = new Date();
+var hourNow = today.getHours();
+var greeting;
+if (hourNow > 18) {
+    greeting = 'Good Evening!';
+} else if (hourNow > 12) {
+    greeting = 'Good Afternoon!';
+} else if (hourNow > 0) {
+    greeting = 'Good Morning!';
+} else {
+    greeting = 'Welcome traveler!';
 }
-function addGreeting() {
-    var today = new Date();
-    var hourNow = today.getHours();
-    var greeting;
-    if (hourNow > 18) {
-        greeting = 'Good Evening!';
-    } else if (hourNow > 12) {
-        greeting = 'Good Afternoon!';
-    } else if (hourNow > 0) {
-        greeting = 'Good Morning!';
-    } else {
-        greeting = 'Welcome traveler!';
-    }
-    return document.write('<h3>' + greeting + '</h3>');
+document.write('<h3>' + greeting + '</h3>');
+
+
+function getCount(){
+  var count = prompt('How many copies of Skyrim do you want?');
+
+  while (isNaN(count) || count === ''){
+    count = prompt('Please enter in a number. We want to get you the correct number.');
+  }
+  return count;
+
 }
-function askPreference() {
-    var preference = prompt('Are you with the imperial or stormcloak?')
-    var message;
-    if (preference === 'imperial') {
-        message = 'the legion can use all the swords it can get!';
-    } 
-    else if (preference === 'stormcloak') {
-        message = 'Leave me alone';
-    }
-     else {
-        message = 'I did not want to know anyway!';
-    }
-    document.write('<h3>' + message + '</h3>')
-    return document.write('<h3>' + message + '</h3>');
+
+function getProduct(){
+  var response = prompt('Would you like to buy our product, yes or no')
+  var item;
+  while (response !== 'yes' && response !== 'no'){
+    response = prompt('Please enter a repy to us.')
+  }
+  if(response === 'yes'){
+    item = '<img scr="legionbanner.jpeg">';
+  }
+  return item;
 }
-function logger() {
-    var message = 'Hello ';
-    return message;
+
+function showOrder(){
+  var result = ''
+  var itemType = getProduct();
+  var total = getCount();
+
+  for (var i = 0; i < total; i++){
+    result = result + '<p>' + itemType + '</p>';
   }
-  logger();
-  function logUsername(username) {
-    var message = 'Hello ' + username + '!';
-    return message;
-  }
-  logUsername('Louis');
-  var loggerOne = function() {
-    var message = 'Hello ';
-    return message;
-  }
-  var logUsernameTwo = function(username){
-    var message = 'Hello ' + username + '!';
-    return message;
-  }
-  var buildCake = function(cakeType, frosting, filling){
-    var message = ('I need a ' + cakeType + ' cake frosted with ' + frosting + ' frosting and filled with ' + filling + '!');
-    return message;
-  }
+
+  return document.write(result);
+}
